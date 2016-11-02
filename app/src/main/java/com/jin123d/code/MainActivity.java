a9f47a8e6f68b0ea2e95f8dce3688862d0a1c8de
靳东升 <dongsheng.jin@hekr.me>
2016-11-02 14:31:10 +0800

    first commit

---
+++ app/src/main/java/com/jin123d/code/MainActivity.java
@@ -0,0 +1,39 @@
+package com.jin123d.code;
+
+import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //shortCuts();
+    }
+
+
+    private void shortCuts() {
+        ShortcutManager shortcutManager = null;
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            shortcutManager = getSystemService(ShortcutManager.class);
+            ShortcutInfo shortcut = new ShortcutInfo.Builder(this, "id1")
+                    .setShortLabel("Web site")
+                    .setLongLabel("qq")
+                    .setIcon(Icon.createWithResource(this, R.mipmap.ic_launcher))
+                    .setIntent(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.github.com/")))
+                    .build();
+
+            shortcutManager.setDynamicShortcuts(Arrays.asList(shortcut));
+        }
+    }
+
+}
